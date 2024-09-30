2854cbcfc964923d06eafa7e77c0f4e4434ca67c
alanPC <alann.maman@gmail.com>
2024-09-30 20:52:29 -0300

    added metrics

--- src/ml_som_experiment.py
+++ src/ml_som_experiment.py
@@ -43,7 +43,85 @@ def filtro_ngbatch_list(data, labels, tol, n_splits):
     return np.hstack([np.concatenate(filtered), np.concatenate(filtered_labels).reshape(-1, 1)])
 
 
-def train_som(train_data, train_data_labels, parameters):
+def generate_train_map_figure(train_filtered, train_label, som):
+    # plotting the distance map as background
+    w_y = []
+    for xx in train_filtered[train_label == 1]:
+        w_y.append(som.winner(xx))  # getting the winner
+    w_y = np.array(w_y)
+    w_y = np.unique(w_y, axis=0)
+
+    w_n = []
+    for xx in train_filtered[train_label == 0]:
+        w_n.append(som.winner(xx))  # getting the winner
+    w_n = np.array(w_n)
+    w_n = np.unique(w_n, axis=0)
+
+    fig = plt.figure(figsize=(6, 6))
+    plt.pcolor(som.distance_map().T, cmap='bone_r')
+    if len(w_y) > 0:
+        plt.scatter(w_y[:, 0] + .5, w_y[:, 1] + .5, color='red', marker='s', s=50, label='RCG', alpha=0.2)
+    if len(w_n) > 0:
+        plt.scatter(w_n[:, 0] + .5, w_n[:, 1] + .5, color='blue', marker='o', s=30, label='RSG', alpha=0.2)
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
+    plt.tight_layout()
+
+    return fig
+
+
+def generate_test_map_figure(test_data, test_data_labels, som):
+    # plotting the distance map as background
+    w_y = []
+    for xx in test_data[test_data_labels == 1]:
+        w_y.append(som.winner(xx))  # getting the winner
+    w_y = np.array(w_y)
+    w_y = np.unique(w_y, axis=0)
+
+    w_n = []
+    for xx in test_data[test_data_labels == 0]:
+        w_n.append(som.winner(xx))  # getting the winner
+    w_n = np.array(w_n)
+    w_n = np.unique(w_n, axis=0)
+
+    fig = plt.figure(figsize=(6, 6))
+    plt.pcolor(som.distance_map().T, cmap='bone_r')
+    if len(w_y) > 0:
+        plt.scatter(w_y[:, 0] + .5, w_y[:, 1] + .5, color='red', marker='s', s=50, label='RCG', alpha=0.2)
+    if len(w_n) > 0:
+        plt.scatter(w_n[:, 0] + .5, w_n[:, 1] + .5, color='blue', marker='o', s=30, label='RSG', alpha=0.2)
+
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.tick_params(left=False, right=False, labelleft=False, labelbottom=False, bottom=False)
+    plt.tight_layout()
+
+    return fig
+
+
+def generate_model_metrics(train_filtered, train_label, som, test_data, test_data_labels, scaler):
+    def classify(som, data):
+        winmap = som.labels_map(train_filtered, train_label)
+        default_class = np.sum(list(winmap.values())).most_common()[0][0]
+        result = []
+        for d in data:
+            win_position = som.winner(d)
+            if win_position in winmap:
+                result.append(winmap[win_position].most_common()[0][0])
+            else:
+                result.append(default_class)
+        return result
+
+    test_data_scaled = scaler.transform(test_data)
+    som_classification = classify(som, test_data_scaled)
+
+    model_metrics = {"f-score": metrics.f1_score(test_data_labels.values, som_classification),
+                     "matthews_corrcoef": metrics.matthews_corrcoef(test_data_labels.values, som_classification)}
+
+    return model_metrics
+
+
+def get_som(train_data, train_data_labels, valid_data, valid_data_labels, test_data, test_data_labels, parameters):
 
     with mlflow.start_run():
         mlflow.log_params(parameters)
@@ -53,27 +131,41 @@ def train_som(train_data, train_data_labels, parameters):
         train_scaled = scaler.fit_transform(train_data)
 
         if parameters['do_ng_filtering']:
-            treino_filtered = filtro_ngbatch_list(train_scaled,
-                                                  train_data_labels.values,
-                                                  parameters['tol'],
-                                                  parameters['n_batches'])
+            filtered = filtro_ngbatch_list(train_scaled,
+                                           train_data_labels.values,
+                                           parameters['tol'],
+                                           parameters['n_batches'])
         else:
-            treino_filtered = train_scaled
+            filtered = train_scaled
+
+        train_filtered = filtered[:, :-1]
+        train_label = filtered[:, -1]
 
         som = MiniSom(parameters['nm'],
                       parameters['nm'],
-                      treino_filtered.shape[1],
+                      train_filtered.shape[1],
                       sigma=parameters['sigma'],
                       learning_rate=parameters['lr'],
                       neighborhood_function='gaussian',
                       activation_distance='euclidean',
                       random_seed=0)
-        som.train(treino_filtered,
+
+        som.train(train_filtered,
                   num_iteration=parameters['num_iteration'],
                   use_epochs=parameters['use_epochs'],
                   verbose=True)
 
-        mlflow.log_metrics({"metric1": 1})
+        train_figure = generate_train_map_figure(train_filtered, train_label, som)
+
+        mlflow.log_figure(train_figure, "train_figure.png")
+
+        test_figure = generate_test_map_figure(test_data.values, test_data_labels.values, som)
+
+        mlflow.log_figure(test_figure, "test_figure.png")
+
+        model_metrics = generate_model_metrics(train_filtered, train_label, som, test_data, test_data_labels, scaler)
+
+        mlflow.log_metrics(model_metrics)
 
 
 def main(data_folder_path, params_path):
@@ -88,7 +180,13 @@ def main(data_folder_path, params_path):
     with open(params_path) as f:
         parameters = json.load(f)
 
-    train_som(train_data=train_data, train_data_labels=train_data_labels, parameters=parameters)
+    get_som(train_data=train_data,
+            train_data_labels=train_data_labels,
+            valid_data=valid_data,
+            valid_data_labels=valid_data_labels,
+            test_data=test_data,
+            test_data_labels=test_data_labels,
+            parameters=parameters)
 
 
 if __name__ == "__main__":
